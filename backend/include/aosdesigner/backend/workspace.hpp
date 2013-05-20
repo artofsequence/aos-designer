497fff79e8acf3b91983724d631e97ef0c880829
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-05-20 11:21:05 +0200

    Added Workspace in backend that will be the root of all backend communications.

---
+++ backend/include/aosdesigner/backend/workspace.hpp
@@ -0,0 +1,25 @@
+#ifndef HGUARD_AOSD_BACKEND_WORKSPACE_HPP__
+#define HGUARD_AOSD_BACKEND_WORKSPACE_HPP__
+
+#include <aosdesigner/backend/api.hpp>
+
+namespace aosd {
+namespace backend {
+
+	class AOSD_BACKEND_API Workspace
+	{
+	public:
+		
+		Workspace();
+		~Workspace();
+
+	private:
+
+
+	};
+
+
+}}
+
+
+#endif
---
+++ backend/source/workspace.cpp
@@ -0,0 +1,19 @@
+#include <aosdesigner/backend/workspace.hpp>
+
+
+
+namespace aosd {
+namespace backend {
+
+	
+	Workspace::Workspace()
+	{
+
+	}
+
+	Workspace::~Workspace()
+	{
+
+	}
+
+}}
