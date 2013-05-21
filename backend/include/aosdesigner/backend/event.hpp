fcf601836845f0d76750b87990b8a41d4fe15141
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-05-21 21:36:12 +0200

    backend: Added event header - WIP

---
+++ backend/include/aosdesigner/backend/event.hpp
@@ -0,0 +1,25 @@
+#ifndef HGUARD_AOSD_BACKEND_EVENT_HPP__
+#define HGUARD_AOSD_BACKEND_EVENT_HPP__
+
+
+
+namespace aosd {
+namespace backend {
+
+	class Event
+	{
+	public:
+		
+		
+
+	private:
+
+
+
+	};
+
+
+}}
+
+
+#endif
