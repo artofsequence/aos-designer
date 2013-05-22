9cc8f9fed1313d5f72202d12eadad1924954ff9a
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-05-23 00:52:07 +0200

    backend: base events implementation

--- backend/include/aosdesigner/backend/event.hpp
+++ backend/include/aosdesigner/backend/event.hpp
@@ -1,25 +1,32 @@
 #ifndef HGUARD_AOSD_BACKEND_EVENT_HPP__
 #define HGUARD_AOSD_BACKEND_EVENT_HPP__
 
-
+#include <aosdesigner/backend/id.hpp>
 
 namespace aosd {
 namespace backend {
+namespace event {
+	
+	template< class EventImpl >
+	struct ProjectEvent { ProjectId project_id; };
 
-	class Event
-	{
-	public:
-		
-		
+	template< class E >
+	struct SequenceEvent : public ProjectEvent<E> 
+	{ SequenceId sequence_id; };
 
-	private:
+	template< class E >
+	struct SessionEvent : public SequenceEvent<E> 
+	{ EditionSessionId edit_id; };
 
+	struct ProjectOpen		: public ProjectEvent<ProjectOpen> {};
+	struct ProjectClosed	: public ProjectEvent<ProjectClosed> {};
 
+	struct SequenceAdded	: public SequenceEvent<SequenceAdded> {};
+	struct SequenceRemoved	: public SequenceEvent<SequenceRemoved> {};
 
-	};
+	struct SessionBegin		: public SessionEvent<SessionBegin> {};
+	struct SessionEnd		: public SessionEvent<SessionEnd> {};
 
-
-}}
-
+}}}
 
 #endif
