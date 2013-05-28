49fc0997f930b5a58013ce3732371aa103a454e2
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-05-28 17:33:20 +0200

    added work queue test from my other project (don't compile yet)

---
+++ backend/test/workqueue.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+
+#include <aosdesigner/backend/workqueue.hpp>
+#include <future>
+
+using namespace aosd::backend;
+
+TEST( Test_WorkQueue, simple_task_sequence )
+{
+	int k = 0;
+	WorkQueue<void> work_queue;
+	work_queue.push( [&]{ k = 42; } );
+	work_queue.execute();
+	work_queue.push( [&]{ k = 666; } );
+
+	ASSERT_EQ( 42, k );
+
+	work_queue.execute();
+
+	ASSERT_EQ( 666, k );
+
+}
+
+
+TEST( Test_WorkQueue, simple_task_sequence_param )
+{
+	int k = 0;
+	WorkQueue<int> work_queue;
+	work_queue.push( [&]{ k = 42; } );
+	work_queue.execute( 123 );
+	work_queue.push( [&]( const int& value ){ k = value; } );
+
+	ASSERT_EQ( 42, k );
+
+	work_queue.execute( 666 );
+
+	ASSERT_EQ( 666, k );
+
+}
+
+int TRUC = 42;
+TEST( Test_WorkQueue, future_promise_shared_work )
+{
+	WorkQueue<void> work_queue;
+
+	auto do_some_work = [&]()-> std::future<int*>
+	{
+		auto promise = std::make_shared<std::promise<int*>>();
+		work_queue.push( [=]
+		{
+			promise->set_value( &TRUC );
+		});
+
+		return promise->get_future();
+	};
+
+	auto ft_value = do_some_work();
+
+	work_queue.execute();
+
+	auto value = ft_value.get();
+	ASSERT_EQ( &TRUC, value );
+}
