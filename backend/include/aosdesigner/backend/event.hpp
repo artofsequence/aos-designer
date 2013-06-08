a0cad0fc90ea1bad9de2fca9cee85d44ec287478
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-08 19:04:22 +0200

    Rewrote event types to easily embedd infos.

--- backend/include/aosdesigner/backend/event.hpp
+++ backend/include/aosdesigner/backend/event.hpp
@@ -2,30 +2,21 @@
 #define HGUARD_AOSD_BACKEND_EVENT_HPP__
 
 #include <aosdesigner/backend/id.hpp>
+#include <aosdesigner/backend/projectinfo.hpp>
+#include <aosdesigner/backend/sequenceinfo.hpp>
 
 namespace aosd {
 namespace backend {
 namespace event {
 	
-	template< class EventImpl >
-	struct ProjectEvent { ProjectId project_id; };
-
-	template< class E >
-	struct SequenceEvent : public ProjectEvent<E> 
-	{ SequenceId sequence_id; };
-
-	template< class E >
-	struct EditorEvent : public SequenceEvent<E> 
-	{ EditorId editor_id; };
-
-	struct ProjectOpen		: public ProjectEvent<ProjectOpen> {};
-	struct ProjectClosed	: public ProjectEvent<ProjectClosed> {};
-
-	struct SequenceAdded	: public SequenceEvent<SequenceAdded> {};
-	struct SequenceRemoved	: public SequenceEvent<SequenceRemoved> {};
-
-	struct EditorBegin		: public EditorEvent<EditorBegin> {};
-	struct EditorEnd		: public EditorEvent<EditorEnd> {};
+	struct ProjectOpen : ProjectInfoEx	{};
+	struct ProjectClosed { ProjectId id; };
+							
+	struct SequenceAdded : SequenceInfo	{  };
+	struct SequenceRemoved	{};
+							
+	struct EditorBegin		{};
+	struct EditorEnd		{};
 
 }}}
 
