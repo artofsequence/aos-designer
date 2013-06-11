990db8511bc9fc68cdcca7d8c350aa7fed7fff66
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-11 21:49:25 +0200

    Redefined basic event for workspace objects existence.

--- backend/include/aosdesigner/backend/event.hpp
+++ backend/include/aosdesigner/backend/event.hpp
@@ -4,19 +4,64 @@
 #include <aosdesigner/backend/id.hpp>
 #include <aosdesigner/backend/projectinfo.hpp>
 #include <aosdesigner/backend/sequenceinfo.hpp>
+#include <aosdesigner/backend/editorinfo.hpp>
+#include <aosdesigner/backend/libraryinfo.hpp>
 
 namespace aosd {
 namespace backend {
 namespace event {
 	
-	struct ProjectOpen : ProjectInfoEx	{};
-	struct ProjectClosed { ProjectId id; };
-							
-	struct SequenceAdded : SequenceInfo	{  };
-	struct SequenceRemoved	{};
-							
-	struct EditorBegin		{};
-	struct EditorEnd		{};
+	struct ProjectOpen
+	{
+		ProjectInfo project_info;
+	};
+
+	struct ProjectClosed 
+	{
+		ProjectId project_id;
+	};
+
+	struct LibraryLoaded
+	{
+		LibraryInfo library_info;
+	};
+
+	struct LibraryUnloaded
+	{
+		LibraryId library_id;
+	};
+	
+	struct SequenceAdded
+	{
+		SequenceInfo sequence_info;
+	};
+
+	struct SequenceRemoved 
+	{
+		ProjectId project_id;
+		SequenceId sequence_id; 
+	};
+
+	struct SequenceLoaded
+	{
+		SequenceInfo sequence_info;
+	};
+
+	struct SequenceUnloaded
+	{
+		SequenceId sequence_id;
+	};
+	
+	struct EditorOpen
+	{
+		EditorInfo editor_info;
+	};
+
+	struct EditorClosed
+	{
+		EditorId editor_id;
+	};
+
 
 }}}
 
