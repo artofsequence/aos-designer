7bf919fd7d61968f686fdd5ada98c53fa2a61edf
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-12 17:08:35 +0200

    Removed id generator mutex, replaced by a ssynchronization facility (which use a mutex but it's simpler to use).

--- backend/source/id.cpp
+++ backend/source/id.cpp
@@ -2,24 +2,20 @@
 
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/nil_generator.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/lock_guard.hpp>
+#include <boost/thread/synchronized_value.hpp>
 
 namespace aosd {
 namespace backend {
 
-	namespace {
-		boost::mutex id_gen_mutex;
-	}
 
 namespace detail {
 	
 	IdValueType generate_random_id()
 	{
-		static boost::uuids::random_generator generator;
-		boost::lock_guard<boost::mutex> _(id_gen_mutex);
-
-		return generator();
+		using namespace boost;
+		static synchronized_value<uuids::random_generator> generator;
+		
+		return generator->operator()();
 	}
 
 }
