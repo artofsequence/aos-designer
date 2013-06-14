1e56727f1cba19292c7e153fddb38e0f0dca72fc
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-14 13:04:12 +0200

    Added Sequence implementation (minimal).

--- backend/source/sequence.cpp
+++ backend/source/sequence.cpp
@@ -3,66 +3,85 @@
 #include <boost/thread/synchronized_value.hpp>
 
 #include <utilcpp/make_unique.hpp>
+#include <aoslcpp/algorithm/edition.hpp>
 
 #include <aosdesigner/backend/project.hpp>
 
-#include <aoslcpp/algorithm/edition.hpp>
 
 namespace aosd {
 namespace backend {
 
+
 	class Sequence::Impl
 	{
 	public:
-		explicit Impl( SequenceInfo info )
-			: m_info( info )
-			, m_aosl( aoslcpp::make_empty_sequence( info.name, to_string(info.id) ) )
-		{
-			if( !is_valid( *m_info ) )
-			{
-				std::stringstream error_message;
-				error_message << "Tried to build a Sequence with invalid informations!"; // TODO: add informations
-				throw std::invalid_argument( error_message.str() );
-			}
-			
-		}
-
-		explicit Impl( SequenceInfo info, aosl::Sequence aosl_data ) // TODO: c++11 constructor...
-			: m_info( info ) 
-			, m_aosl( std::move(aosl_data) )
-		{  
-			if( !is_valid( info ) )
-			{
-				std::stringstream error_message;
-				error_message << "Tried to build a Sequence with invalid informations!"; // TODO: add informations
-				throw std::invalid_argument( error_message.str() );
-			}
-
-			auto aosl_source = m_aosl.synchronize();
-			aosl_source->id( aosl::Unique_id(to_string(info.id)) );
-			aosl_source->name( info.name );
-
-		}
+		explicit Impl( Sequence& sequence, SequenceInfo info );
+		explicit Impl( Sequence& sequence, SequenceInfo info, aosl::Sequence aosl_data );
 
 		SequenceInfo info() const { return *m_info; }
 		aosl::Sequence aosl_source() const { return *m_aosl; }
 
 	private:
+		Impl( const Impl& ); // = delete;
+		Impl& operator=( const Impl& ); // = delete;
+
+		Sequence& m_sequence;
 		boost::synchronized_value<SequenceInfo> m_info;
 		boost::synchronized_value<aosl::Sequence> m_aosl;
 
+		static aosl::Sequence merge_info_in_aosl( aosl::Sequence aosl_source, const SequenceInfo& info );
+
 	};
+
+	Sequence::Impl::Impl( Sequence& sequence, SequenceInfo info )
+		: m_info( info )
+		, m_aosl( aoslcpp::make_empty_sequence( info.name, to_string(info.id) ) )
+		, m_sequence( sequence )
+	{
+		if( !is_valid( *m_info ) )
+		{
+			std::stringstream error_message;
+			error_message << "Tried to build a Sequence with invalid informations!"; // TODO: add informations
+			throw std::invalid_argument( error_message.str() );
+		}
+
+	}
+
+	Sequence::Impl::Impl( Sequence& sequence, SequenceInfo info, aosl::Sequence aosl_data ) // TODO: c++11 constructor...
+		: m_info( info ) 
+		, m_aosl( merge_info_in_aosl( aosl_data, info ) )
+		, m_sequence( sequence )
+	{  
+		if( !is_valid( info ) )
+		{
+			std::stringstream error_message;
+			error_message << "Tried to build a Sequence with invalid informations!"; // TODO: add informations
+			throw std::invalid_argument( error_message.str() );
+		}
+
+	}
+
+	aosl::Sequence Sequence::Impl::merge_info_in_aosl( aosl::Sequence aosl_source, const SequenceInfo& info )
+	{
+		aosl_source.id( aosl::Unique_id(to_string(info.id)) );
+		aosl_source.name( info.name );
+		return aosl_source;
+	}
+
+
+	///////////////////////////////////
+
 
 	Sequence::Sequence( Workspace& workspace, SequenceInfo info )
 		: WorkspaceObject( workspace, info.id )
-		, impl( std::make_unique<Impl>( std::move(info) ) )
+		, impl( std::make_unique<Impl>( *this, std::move(info) ) )
 	{
 
 	}
 
 	Sequence::Sequence( Workspace& workspace, SequenceInfo info, aosl::Sequence aosl_data )
 		: WorkspaceObject( workspace, info.id )
-		, impl( std::make_unique<Impl>( std::move(info), std::move(aosl_data) ) ) 
+		, impl( std::make_unique<Impl>( *this, std::move(info), std::move(aosl_data) ) ) 
 	{
 
 	}
@@ -81,7 +100,6 @@ namespace backend {
 	{
 		return impl->aosl_source();
 	}
-
 
 
 
