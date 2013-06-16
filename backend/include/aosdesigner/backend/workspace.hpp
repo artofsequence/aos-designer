db8ba70a63b6f88205dcad1ad1b84b12d7138d96
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-16 20:14:52 +0200

    Fixed: Workspace::work_on can't create a optional<future> (not possible for now) - will return true if the provided action will be executed.

--- backend/include/aosdesigner/backend/workspace.hpp
+++ backend/include/aosdesigner/backend/workspace.hpp
@@ -16,6 +16,12 @@
 namespace aosd {
 namespace backend {
 
+	/** The work space in which all projects manipulations will occur.
+		This is the root object to manipulate all the rest of AOS Designer's backend.
+		Manage open projects, dispatch events and update everything automatically when something have changed.
+		
+		
+	*/
 	class AOSD_BACKEND_API Workspace
 	{
 	public:
@@ -33,8 +39,7 @@ namespace backend {
 
 		// project, sequence, editor, library thread-safe access - must occur in the object's update.
 		template< class ObjectType, class TaskType >
-		auto work_on( const Id<ObjectType>& id, TaskType task, ObjectType* dummy = nullptr ) 
-			-> boost::optional< future< decltype(task(*dummy)) > >;
+		future<bool> work_on( const Id<ObjectType>& id, TaskType task );
 
 		//SequenceInfo get_sequence_info( SequenceId sequence_id );
 		//ProjectInfo get_project_info( ProjectId project_id );
@@ -70,22 +75,26 @@ namespace backend {
 		std::shared_ptr<Library> find( const LibraryId& id ) const;
 		std::shared_ptr<Sequence> find( const SequenceId& id ) const;
 
+		template< class ObjectType > 
+		static ObjectType* dummy() { return nullptr; }
+
 	};
 
 	template< class ObjectType, class TaskType >
-	auto Workspace::work_on( const Id<ObjectType>& id, TaskType task, ObjectType* dummy )
-		-> boost::optional< future< decltype(task(*dummy)) > >
+	future<bool> Workspace::work_on( const Id<ObjectType>& id, TaskType task )
 	{
-		static_assert( std::is_base_of< WorkspaceObject<ObjectType>, ObjectType>::value, "Tried to work on an object of type unknown to Workspace!"
+		static_assert( std::is_base_of< WorkspaceObject<ObjectType>, ObjectType>::value, "Tried to work on an object of type unknown to Workspace! "
 			"Valid types: Project, Sequence, Library, Editor" );
 		if( auto object = find( id ) )
 		{
 			WorkspaceObject<ObjectType>& workspace_object = static_cast<WorkspaceObject<ObjectType>&>( *object );
 			return workspace_object.on_next_update( [=]( ObjectType& ready_object ){ 
 				task( ready_object ); 
+				return true;
 			});
 		}
-		return boost::none;
+
+		return make_ready_future(false);
 	}
 
 }}
--- backend/test/workspace.cpp
+++ backend/test/workspace.cpp
@@ -7,24 +7,14 @@ using namespace aosd::backend;
 
 TEST( Test_Workspace, access_object )
 {
-	Workspace workspace( (TaskExecutor_Immediate()) );
+	TaskExecutor_Immediate executor;
+	Workspace workspace( executor );
 	
 	bool accessed_nonexisting_project = false;
-	auto opt_ft = workspace.work_on( make_new_id<Project>(), [&]( Project& project ){ accessed_nonexisting_project = true; } );
+	auto access_done = workspace.work_on( make_new_id<Project>(), [&]( Project& project ){ accessed_nonexisting_project = true; } );
 	ASSERT_FALSE( accessed_nonexisting_project );
-	ASSERT_FALSE( opt_ft.is_initialized() );
+	ASSERT_FALSE( access_done.get() );
 
 	// TODO: find a way to test if you can access an object that do exists
 
 }
-
-//
-//#include <boost/optional.hpp>
-//#include <future>
-//
-//boost::optional< std::future<void> > top() { return std::async( []{ int i=0; ++i;  } ); }
-//
-//int main()
-//{
-//	auto opt = top();
-//}
