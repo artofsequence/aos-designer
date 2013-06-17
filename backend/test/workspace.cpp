8abc7c19f66e6fb8c98593ed17e69e8f2584282c
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-17 12:34:10 +0200

    Added test for checking that opening a project triggers an event.

--- backend/test/workspace.cpp
+++ backend/test/workspace.cpp
@@ -2,6 +2,7 @@
 
 #include <aosdesigner/backend/workspace.hpp>
 #include <aosdesigner/backend/project.hpp>
+#include <aosdesigner/backend/event.hpp>
 
 using namespace aosd::backend;
 
@@ -17,7 +18,7 @@ TEST( Test_Workspace, access_project_failure )
 	
 }
 
-TEST( Test_Workspace, access_project_success )
+TEST( Test_Workspace, open_project )
 {
 	TaskExecutor_Immediate executor;
 	Workspace workspace( executor );
@@ -25,8 +26,17 @@ TEST( Test_Workspace, access_project_success )
 	ProjectInfo project_info;
 	project_info.id = make_new_id<Project>();
 	project_info.name = "Unnamed project";
-	workspace.open_project( project_info );
 	
+	bool got_the_global_message = false;
+	bool got_the_specific_message = false;
+	workspace.event_dispatcher().on<event::ProjectOpen>( [&]{ got_the_global_message = true; } );
+	workspace.event_dispatcher().on<event::ProjectOpen>( project_info.id, [&]{ got_the_specific_message = true; } );
+
+	workspace.open_project( project_info );
+	workspace.dispatch_events();
+	ASSERT_TRUE( got_the_global_message );
+	ASSERT_TRUE( got_the_specific_message );
+
 	bool accessed_project = false;
 	auto access_done = workspace.work_on( project_info.id, [&]( Project& project ){ 
 		ASSERT_EQ( project_info.id, project.id() );
