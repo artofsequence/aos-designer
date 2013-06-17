eaeadd6925f663909e2040e0f3a6c974ad80bb09
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-17 12:06:11 +0200

    Added test for openning a project from a Workspace.

--- backend/test/workspace.cpp
+++ backend/test/workspace.cpp
@@ -5,7 +5,7 @@
 
 using namespace aosd::backend;
 
-TEST( Test_Workspace, access_object )
+TEST( Test_Workspace, access_project_failure )
 {
 	TaskExecutor_Immediate executor;
 	Workspace workspace( executor );
@@ -14,7 +14,27 @@ TEST( Test_Workspace, access_object )
 	auto access_done = workspace.work_on( make_new_id<Project>(), [&]( Project& project ){ accessed_nonexisting_project = true; } );
 	ASSERT_FALSE( accessed_nonexisting_project );
 	ASSERT_FALSE( access_done.get() );
+	
+}
 
-	// TODO: find a way to test if you can access an object that do exists
+TEST( Test_Workspace, access_project_success )
+{
+	TaskExecutor_Immediate executor;
+	Workspace workspace( executor );
+
+	ProjectInfo project_info;
+	project_info.id = make_new_id<Project>();
+	project_info.name = "Unnamed project";
+	workspace.open_project( project_info );
+	
+	bool accessed_project = false;
+	auto access_done = workspace.work_on( project_info.id, [&]( Project& project ){ 
+		ASSERT_EQ( project_info.id, project.id() );
+		accessed_project = true;
+	});
+
+	ASSERT_TRUE( accessed_project );
+	ASSERT_TRUE( access_done.get() );
 
 }
+
