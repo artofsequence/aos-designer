897e5b2b4eb6db5672b329d9ce57cfb2956eae1d
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-20 12:39:17 +0200

    Fixed missing interface.

--- backend/source/workspaceinternalapi.hpp
+++ backend/source/workspaceinternalapi.hpp
@@ -2,7 +2,10 @@
 #define HGUARD_AOSD_BACKEND_WORKSPACEINTERNALAPI_HPP__
 
 #include <memory>
+#include <boost/thread/mutex.hpp>
+
 #include <aosdesigner/backend/workspace.hpp>
+#include <aosdesigner/backend/tools/uri.hpp>
 
 namespace aosd {
 namespace backend {
@@ -18,9 +21,12 @@ namespace backend {
 		void add_to_registry( std::shared_ptr<Editor> sequence );
 		void add_to_registry( std::shared_ptr<Library> sequence );
 
+		std::shared_ptr<Sequence> find_or_load( const SequenceId& id, const URI& location );
+		std::shared_ptr<Library> find_or_load( const LibraryId& id, const URI& location );
+
 	private:
 		Workspace::Impl& m_workspace_impl;
-
+		
 	};
 
 
