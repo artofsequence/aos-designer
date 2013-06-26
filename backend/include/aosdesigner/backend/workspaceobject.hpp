30c6f9a065b4bd397972d1983072e1cf9d7be4a5
Klaim - A. Joel Lamotte <mjklaim@gmail.com>
2013-06-26 11:24:36 +0200

    Added comments in WorkspaceObject interface.

--- backend/include/aosdesigner/backend/workspaceobject.hpp
+++ backend/include/aosdesigner/backend/workspaceobject.hpp
@@ -31,15 +31,53 @@ namespace backend {
 		/** @return Unique identifier of this object. */
 		const Id<T>& id() const { return m_id; }
 
+		/** Event handling connection.
+			The observer will get events which source is specific object.
+			The events are processed by the Workspace EventQueueDispatcher,
+			this is only a shortcut that can be used if you already have access to the object.
+			@see EventQueueDispatcher for details.
+
+			Usage (with a fictive event):
+			@code
+				workspace_object.on<UpdateEvent>( []( const UpdateEvent& ev ){ 
+					log( "Object Updated: " << ev.object_name );
+				});
+
+				// this is equivalent to the previous code
+
+				workspace.event_dispatcher().on<UpdateEvent>( object_id, []( const UpdateEvent& ev ){ 
+					log( "Object Updated: " << ev.object_name );
+				});
+			@endcode
+
+			@param observer		An callable object which can take the EventType as argument.
+			@return A connection object, used to disconnect the observer later if necessary.
+		*/
 		template< class EventType, class ObserverType >
 		EventDispatcher::Connection on( ObserverType&& observer );
 
+		/** @return Access to the object's workspace. */
 		Workspace& workspace() { return m_workspace; }
-
+		
+		/** Add a task to be executed on the next object's update cycle.
+			The task will be executed in sync with the update.
+			@param task				Callable object which can take this object's reference as parameter. task( T& )
+			@return The result of task once executed.
+		*/
 		template< class TaskType >
 		auto on_next_update( TaskType task ) -> future< decltype(task( *dummy() )) >;
 
+		/** Update cycle: Execute scheduled tasks using the calling thread.
+			@warning This should only be called by the system managing this object (most of the time the Workspace), 
+			in only one thread at a time.
+		*/
+		void update();
+
 	protected:
+		/** Base constructor.
+			@param workspace		Workspace this object will be managed by.
+			@param new_id			Unique id of the object. Have to be valid.
+		*/
 		explicit WorkspaceObject( Workspace& workspace, Id<T> new_id )
 			: m_workspace( workspace )
 			, m_id( std::move(new_id) )
@@ -56,15 +94,26 @@ namespace backend {
 
 		virtual ~WorkspaceObject(){} // = default;
 
+		/** Add a task to be executed on the next object's update cycle and request an update.
+			@remark If no update cycle is scheduled, calling this will request/trigger one.
+			@param task				Callable object which takes no argument.
+			@return The result of the task once executed.
+		*/
 		template< class TaskType >
 		auto schedule( TaskType&& task ) -> future< decltype(task()) >;
 
+		/** Execute a task asynchronously by using the Workspace's executor.
+			@remark The task will not be executed in sync with this object's update.
+			@param task				Callable object which takes no argument.
+			@return The result of the task once executed.
+		*/
 		template< class TaskType >
 		auto async( TaskType&& task ) -> future< decltype(task()) >;
-
-		friend class Workspace::Impl;
-		void update();
 		
+		/** Publish an event.
+			The event will be pushed into the Workspace's EventQueueDispatcher with this
+			object id as being the source.
+		*/
 		template< class EventType >
 		void publish( EventType&& e );
 		
